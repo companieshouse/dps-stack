3b51f130a716f34008c13224f0d4fe401af9572f
Marc Ransome <mransome@companieshouse.gov.uk>
2024-09-10 15:28:45 +0100

    Enforce latest TLS policy for ALB

--- groups/dps/alb.tf
+++ groups/dps/alb.tf
@@ -52,7 +52,7 @@ resource "aws_lb_listener" "qa_app_https" {
   load_balancer_arn = aws_lb.qa_app.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = aws_acm_certificate_validation.qa_app.certificate_arn
 
   default_action {
